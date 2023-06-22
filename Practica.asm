d18a140b82bef5dbddfe2c03611f6ebd15906ef6
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-06-22 11:01:17 -0600

    encabezado, macro print

--- Practica.asm
+++ Practica.asm
@@ -1,45 +1,40 @@
-;; DIRECTIVAS
 .MODEL SMALL
 .RADIX 16
 .STACK
-;; PILA
 .DATA
-;; VARIABLES | MEMORIA RAM
-;; tamaño byte (db)
-universidad	db "Universidad de San Carlos de Guatemala", 0a, "$"
-facultad  	db "Facultad de Ingenier", 0a1, "a", 0a, "$"
-escuela 	db "Escuela de Vacaciones", 0a, "$"
-curso  		db "Arquitectura de Computadoras y Ensambladores 1", 0a, 0a, "$"
-nombre  	db "Nombre: Danny Hugo Bryan Tejax", 0a3, "n Pichiy", 0a0, 0a, "$"
-carnet  	db "Carne: 201908355", 0a, "$"
+	universidad				db				"Universidad de San Carlos de Guatemala", "$"
+	facultad  				db				"Facultad de Ingenier", 0a1, "a", "$"
+	escuela 				db				"Escuela de Vacaciones", "$"
+	curso  					db				"Arquitectura de Computadoras y Ensambladores 1", "$"
+	nombre  				db				"Nombre: Danny Hugo Bryan Tejax", 0a3, "n Pichiy", 0a0, "$"
+	carnet  				db				"Carne: 201908355", "$"
+	ln						db				0a, "$"
 .CODE
 .STARTUP
-;; CÓDIGO
-inicio:
-	;; print de string
-	mov DX, offset universidad
-	mov AH, 09
-	int 21
-	;; print de string
-	mov DX, offset facultad
-	mov AH, 09
-	int 21
-	;; print de string
-	mov DX, offset escuela
-	mov AH, 09
-	int 21
-	;; print de string
-	mov DX, offset curso
-	mov AH, 09
-	int 21
-	;; print de string
-	mov DX, offset nombre
-	mov AH, 09
-	int 21
-	;; print de string
-	mov DX, offset carnet
-	mov AH, 09
-	int 21
-fin:
+main:
+	println macro text
+		mov DX, offset text
+		mov AH, 09
+		int 21
+		mov DX, offset ln
+		mov AH, 09
+		int 21
+	endm
+
+	print macro text
+		mov DX, offset text
+		mov AH, 09
+		int 21
+	endm
+
+	encabezado:
+		println universidad
+		println facultad
+		println escuela
+		println curso
+		print ln
+		println nombre
+		println carnet
+end:
 .EXIT
 END
