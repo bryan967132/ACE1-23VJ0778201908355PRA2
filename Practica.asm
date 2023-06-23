cc06fdf956aa364338c3c95006c55f714390a1a0
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-06-22 19:31:13 -0600

    Etiquetas archivos, handles, Producto

--- Practica.asm
+++ Practica.asm
@@ -2,26 +2,26 @@
 .RADIX 16
 .STACK
 .DATA
-	; DATOS DE ENCABEZADO
-	universidad				db				0ba, " Universidad de San Carlos de Guatemala         ", 0ba, "$"
-	facultad  				db				0ba, " Facultad de Ingenieria                         ", 0ba, "$"
-	escuela 				db				0ba, " Escuela de Vacaciones                          ", 0ba, "$"
-	curso  					db				0ba, " Arquitectura de Computadoras y Ensambladores 1 ", 0ba, "$"
-	nombre  				db				0ba, " Nombre: Danny Hugo Bryan Tejaxun Pichiya       ", 0ba, "$"
-	carnet  				db				0ba, " Carne: 201908355                               ", 0ba, "$"
 	; LÍNEAS
-	line					db				0a, "$"
-	cornUL					db				0c9, "$"
-	cornDL					db				0c8, "$"
-	middleL					db				0cc, "$"
-	cornUR					db				0bb, "$"
-	cornDR					db				0bc, "$"
-	middleR					db				0b9, "$"
-	lineL					db				30 dup (0cd), "$"
+	line                    db				0a, "$"
+	cornUL                  db				0c9, "$"
+	cornDL                  db				0c8, "$"
+	middleL                 db				0cc, "$"
+	cornUR                  db				0bb, "$"
+	cornDR                  db				0bc, "$"
+	middleR                 db				0b9, "$"
+	lineL                   db				30 dup (0cd), "$"
 	; PROMPT
 	prompt					db				"Seleccione una Opcion", "$"
 	; FINALIZADO
 	finalizado				db				0ad, "Finalizado!", "$"
+	; DATOS DE ENCABEZADO
+	universidad             db				0ba, " Universidad de San Carlos de Guatemala         ", 0ba, "$"
+	facultad                db				0ba, " Facultad de Ingenieria                         ", 0ba, "$"
+	escuela                 db				0ba, " Escuela de Vacaciones                          ", 0ba, "$"
+	curso                   db				0ba, " Arquitectura de Computadoras y Ensambladores 1 ", 0ba, "$"
+	nombre                  db				0ba, " Nombre: Danny Hugo Bryan Tejaxun Pichiya       ", 0ba, "$"
+	carnet                  db				0ba, " Carne: 201908355                               ", 0ba, "$"
 	; OPCIONES MENÚ PRINCIPAL
 	tituloMenuP				db				0ba, "                 Menu Principal                 ", 0ba, "$"
 	tituloProductos			db				0ba, " 1. Productos                                   ", 0ba, "$"
@@ -45,7 +45,27 @@
 	tituloRepoVenH			db				0ba, " 3. Reporte De Ventas                           ", 0ba, "$"
 	tituloRepoAgotH			db				0ba, " 4. Reporte De Productos Agotados               ", 0ba, "$"
 	tituloVolverH			db				0ba, " 5. Volver                                      ", 0ba, "$"
-	;
+	; ARCHIVOS
+	f_productos				db				"PROD.BIN", 00
+	f_ventas				db				"VENT.BIN", 00
+	f_repcatalogo			db				"CATALG.HTM", 00
+	f_repalfabetico			db				"ABC.HTM", 00
+	f_repventas				db				"REP.TXT", 00
+	f_repfalta				db				"FALTA.HTM", 00
+	; HANDLES
+	h_productos				dw				0000
+	h_ventas				dw				0000
+	h_repcatalogo			dw				0000
+	h_repalfabetico			dw				0000
+	h_repventas				dw				0000
+	h_repfalta				dw				0000
+	; ESTRUCTURA PRODUCTO
+	p_codigo				db				05 dup (0)
+	p_nombre				db				05 dup (0)
+	p_precio				db				05 dup (0)
+	p_unidades				db				05 dup (0)
+	n_price					dw				0000
+	n_units					dw				0000
 .CODE
 .STARTUP
 main:
@@ -185,6 +205,7 @@ main:
 		cmp AL, 34 ; OPCION 4: VOLVER
 		je menuPrincipal
 		jmp menuProductos
+
 	; --- MENÚ VENTAS
 	menuVentas:
 		imprimirMenuVentas
@@ -193,6 +214,7 @@ main:
 		cmp AL, 32 ; OPCION 2: VOLVER
 		je menuPrincipal
 		jmp menuVentas
+
 	; --- MENÚ HERRAMIENTAS
 	menuHerramientas:
 		imprimirMenuHerramientas
